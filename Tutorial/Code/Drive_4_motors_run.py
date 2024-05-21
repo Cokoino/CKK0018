aa92388b7c403be117391a9bd3ba0215ed3904e3
cokoino <37071399+Cokoino@users.noreply.github.com>
2024-05-21 13:50:20 +0800

    Convert Word to PDF

--- Tutorial/Code/Drive_4_motors_run.py
+++ Tutorial/Code/Drive_4_motors_run.py
@@ -4,11 +4,13 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-NSLEEP1 = 12
+#define the pin for drv8833#1
+NSLEEP1 = 12  #Enabling signal pin for drv8833
 AN11 = 17
 AN12 = 27
 BN11 = 22
 BN12 = 23
+#define the pin for drv8833#2
 NSLEEP2 = 13
 AN21 = 24
 AN22 = 25
@@ -17,6 +19,7 @@ BN22 = 16
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
+#Define pin as output signal
 GPIO.setup(NSLEEP1,GPIO.OUT)
 GPIO.setup(NSLEEP2,GPIO.OUT)
 GPIO.setup(AN11,GPIO.OUT)
@@ -27,6 +30,7 @@ GPIO.setup(AN21,GPIO.OUT)
 GPIO.setup(AN22,GPIO.OUT)
 GPIO.setup(BN21,GPIO.OUT)
 GPIO.setup(BN22,GPIO.OUT)
+#Initialize the motor drive signal so that the motor is in a stopped state
 GPIO.output(AN11,GPIO.LOW)
 GPIO.output(AN12,GPIO.LOW)
 GPIO.output(BN11,GPIO.LOW)
@@ -35,10 +39,10 @@ GPIO.output(AN21,GPIO.LOW)
 GPIO.output(AN22,GPIO.LOW)
 GPIO.output(BN21,GPIO.LOW)
 GPIO.output(BN22,GPIO.LOW)
-p1=GPIO.PWM(NSLEEP1,1000)
-p2=GPIO.PWM(NSLEEP2,1000)
-p1.start(25)
-p2.start(25)
+p1=GPIO.PWM(NSLEEP1,1000)#Define p1 as a pulse signal of 1000 Hz
+p2=GPIO.PWM(NSLEEP2,1000)#Define p2 as a pulse signal of 1000 Hz
+p1.start(30)#P1 defaults to a duty cycle of 30%
+p2.start(30)#P2 defaults to a duty cycle of 30%
 
 print("\n")
 print("The default speed & direction of motor is LOW & Forward rotation")
@@ -115,18 +119,18 @@ while(1):
         
     elif x=='l':
         print("low")
-        p1.ChangeDutyCycle(30)
-        p2.ChangeDutyCycle(30)
+        p1.ChangeDutyCycle(30)#Set the P1 pulse signal duty cycle to 30%
+        p2.ChangeDutyCycle(30)#Set the P2 pulse signal duty cycle to 30%
         x='z'
     elif x=='m':
         print("medium")
-        p1.ChangeDutyCycle(60)
-        p2.ChangeDutyCycle(60)
+        p1.ChangeDutyCycle(60)#Set the P1 pulse signal duty cycle to 60%
+        p2.ChangeDutyCycle(60)#Set the P2 pulse signal duty cycle to 60%
         x='z'
     elif x=='h':
         print("high")
-        p1.ChangeDutyCycle(90)
-        p2.ChangeDutyCycle(90)
+        p1.ChangeDutyCycle(90)#Set the P1 pulse signal duty cycle to 90%
+        p2.ChangeDutyCycle(90)#Set the P2 pulse signal duty cycle to 90%
         x='z'
     elif x=='e':
         GPIO.cleanup()
